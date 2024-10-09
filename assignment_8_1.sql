3e17fb73eae0bf48c17449fbb954d14a3cf68fdd
xenoyle <93809607+xenoyle@users.noreply.github.com>
2024-10-09 12:06:59 -0400

    assignment 8.1

---
+++ assignment_8_1.sql
@@ -0,0 +1,30 @@
+/*
+	Developer:	Floyd, Connor
+	Semester: Fall 2024
+	Course: B320-001
+	Assignment: Assignment 8.1 - Programming Exercises (Ch. 7)
+*/
+
+-- Exercise 1
+--SELECT *
+--INTO VendorCopy
+--FROM Vendors
+
+--SELECT *
+--INTO InvoiceCopy
+--FROM Invoices
+
+-- Exercise 2
+--INSERT INTO InvoiceCopy (
+--	VendorID,		InvoiceTotal,
+--	TermsID,		InvoiceNumber,
+--	PaymentTotal,	InvoiceDueDate,
+--	InvoiceDate,	CreditTotal,
+--	PaymentDate)
+--VALUES (
+--	32,				434.58,
+--	2,				'AX-014-027',
+--	0.00,			'05-08-2023',
+--	'04-21-2023',	0.00,
+--	NULL)
+
